e9a264606bbd515b7f92195ca6ba22d67ed6c212
Qaraqol <163067984+qaraqol@users.noreply.github.com>
2024-10-14 00:10:38 +0430

    Update repair.js
    Bug fixes

--- repair.js
+++ repair.js
@@ -1,25 +1,3 @@
-import { $ } from "bun";
-import amqp from "amqplib";
-//Must have a missing.json and the first batch (start, end) should be specified,
-//It should be in the below pattern so the ./hyp-repair can read it
-//[{ "start": 330002000, "end": 330002000 }]
-//For the repair tool to work, you need to remove two lines from hyp-repair.js, line #668 and #669:
-// process.stdout.clearLine(0);
-// process.stdout.cursorTo(0);
-const config = {
-  batch: 1500, //how many blocks should be repaired each time the program restarts
-  end: 329173500, //when to finish repairing blocks
-  queueName: "wax:ds_pool:1",
-  amqHost: "localhost",
-  amqUser: "",
-  amqPass: "",
-  vhost: "hyperion",
-  interval: 60000, //interval to recheck the rabbitmq queues and rerun the program in ms.
-};
-
-const file = Bun.file(`missing.json`);
-const range = await file.json();
-
 async function checkQueueAndRepair() {
   if (range[0].end > config.end) {
     console.log("Range finished");
@@ -42,12 +20,12 @@ async function checkQueueAndRepair() {
       console.log(`Messages in queue: ${messageCount}`);
 
       // If there are fewer than 3 messages in the queue, run the repair
-      if (messageCount < 3 && range[0].start < config.end) {
-        range[0].start += config.batch;
-        range[0].end += config.batch;
+      if (messageCount < 3) {
         console.log("****\nStarting a new repair job...");
         console.log("Current Range: " + range[0].start + " - " + range[0].end); //Current range being repaired
         await $`./hyp-repair fill-missing wax missing.json`; //Run the repair command
+        range[0].start += config.batch;
+        range[0].end += config.batch;
         await Bun.write(file, JSON.stringify(range, null, 2)); //Update the missing.json file with the last repaired range
 
         setTimeout(checkQueueAndRepair, config.interval);
@@ -62,5 +40,3 @@ async function checkQueueAndRepair() {
     }
   }
 }
-
-checkQueueAndRepair();
